b5fc6c176374cafc5b5eee2f24e68a3be5737f28
outland.karasu <outland.karasu@gmail.com>
2020-04-15 22:32:53 +0900

    issue #12 add event handler.

--- source/app.d
+++ source/app.d
@@ -1,13 +1,31 @@
 import std.stdio : writefln;
 
-import dearth.sdl : duringSDL, duringWindow;
+import bindbc.sdl : SDL_QuitEvent;
+
+import dearth.sdl : duringSDL, duringWindow, handleEvents;
+
+struct EventHandler
+{
+    void opCall(scope ref const(SDL_QuitEvent) event)
+    {
+        writefln("quit");
+    }
+
+    void opCall(E)(scope ref const(E) event)
+    {
+    }
+}
 
 void main()
 {
     duringSDL((support)
     {
         writefln("%s", support);
-        duringWindow("", 0, 0, 640, 480, (w) {});
+        duringWindow("", 0, 0, 640, 480, (w)
+        {
+            EventHandler handler;
+            handleEvents(handler, () {});
+        });
     });
 }
 
--- source/dearth/sdl/event.d
+++ source/dearth/sdl/event.d
@@ -3,3 +3,39 @@ SDL2 event module.
 */
 module dearth.sdl.event;
 
+import bindbc.sdl :
+    SDL_Event,
+    SDL_QUIT,
+    SDL_PollEvent;
+
+/**
+Handle SDL events.
+
+Params:
+    H = event handler type.
+    Dg = frame draw delegate type.
+    handler = event handler.
+    dg = frame draw delegate.
+*/
+void handleEvents(H, Dg)(scope H handler, scope Dg dg)
+{
+    for (SDL_Event event; ;)
+    {
+        // processing pushed events.
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type) {
+                case SDL_QUIT:
+                    handler(event.quit);
+                    return;
+                default:
+                    handler(event);
+                    break;
+            }
+        }
+
+        // draw a frame.
+        dg();
+    }
+}
+
