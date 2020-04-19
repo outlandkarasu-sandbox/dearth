f27a38671a8d3094c19341aff8a3ca654d24465c
outland.karasu <outland.karasu@gmail.com>
2020-04-19 23:23:29 +0900

    issue #18 add window clear.

--- source/app.d
+++ source/app.d
@@ -1,6 +1,15 @@
 import std.stdio : writefln;
 
 import bindbc.sdl : SDL_QuitEvent;
+import bindbc.opengl :
+    GL_COLOR_BUFFER_BIT,
+    GL_DEPTH_BUFFER_BIT,
+    GL_DEPTH_TEST;
+import bindbc.opengl :
+    glClearColor,
+    glClear,
+    glEnable,
+    glViewport;
 
 import dearth :
     createFragmentShader,
@@ -30,14 +39,24 @@ void main()
             duringOpenGL((glSupport)
             {
                 writefln("%s,%s", sdlSupport, glSupport);
+
+                glViewport(0, 0, 640, 480);
+                glEnable(GL_DEPTH_TEST);
+
                 auto vertexShader = createVertexShader(import("earth.vert"));
                 auto fragmentShader = createFragmentShader(import("earth.frag"));
                 auto shaderProgram = createProgram!Vertex(vertexShader, fragmentShader);
                 auto vao = createVAO!Vertex();
                 scope mainLoop = new MainLoop();
-                mainLoop.run(window);
+                mainLoop.onDraw(() => draw()).run(window);
             });
         });
     });
+}
+
+void draw()
+{
+    glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
