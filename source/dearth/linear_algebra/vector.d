630c80009471e0506b47ef30f07b1c1a1e88741b
outland.karasu <outland.karasu@gmail.com>
2020-06-27 15:44:45 +0900

    issue #22 add opIndexAssign.

--- source/dearth/linear_algebra/vector.d
+++ source/dearth/linear_algebra/vector.d
@@ -27,10 +27,25 @@ struct Vector(size_t D, E = float)
     Returns:
         element value.
     */
-    E opIndex(size_t i) const nothrow pure scope
+    ref const(E) opIndex(size_t i) const nothrow pure return scope
     in (i < D)
     {
         return elements_[i];
+    }
+
+    /**
+    Set an element.
+
+    Params:
+        value = element value.
+        i = index.
+    Returns:
+        assigned element value.
+    */
+    ref const(E) opIndexAssign()(auto ref const(E) value, size_t i) nothrow pure return scope
+    in (i < D)
+    {
+        return elements_[i] = value;
     }
 
 private:
@@ -46,5 +61,20 @@ nothrow pure unittest
     assert(v[0].isClose(1.0));
     assert(v[1].isClose(2.0));
     assert(v[2].isClose(3.0));
+}
+
+///
+nothrow pure unittest
+{
+    import std.math : isClose;
+
+    auto v = Vector!3([1, 2, 3]);
+    v[0] = 2.0f;
+    v[1] = 3.0f;
+    v[2] = 4.0f;
+
+    assert(v[0].isClose(2.0));
+    assert(v[1].isClose(3.0));
+    assert(v[2].isClose(4.0));
 }
 
