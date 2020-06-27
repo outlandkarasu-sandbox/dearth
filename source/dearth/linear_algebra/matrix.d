bc834a22166f74f8a3f08525dec22b02b5033392
outland.karasu <outland.karasu@gmail.com>
2020-06-27 16:25:27 +0900

    issue #22 implement opIndexAssign.

--- source/dearth/linear_algebra/matrix.d
+++ source/dearth/linear_algebra/matrix.d
@@ -41,6 +41,23 @@ struct Matrix(size_t ROWS, size_t COLS, E = float)
         return elements_[i][j];
     }
 
+    /**
+    Set an element.
+
+    Params:
+        value = element value.
+        i = row index.
+        j = column index.
+    Returns:
+        assigned element value.
+    */
+    ref const(E) opIndexAssign()(auto ref const(E) value, size_t i, size_t j) return scope
+    in (i < ROWS)
+    in (j < COLS)
+    {
+        return elements_[i][j] = value;
+    }
+
 private:
     E[COLS][ROWS] elements_;
 }
@@ -63,5 +80,25 @@ private:
     assert(m[1, 0].isClose(4));
     assert(m[1, 1].isClose(5));
     assert(m[1, 2].isClose(6));
+}
+
+///
+@nogc nothrow pure @safe unittest
+{
+    import std.math : isClose;
+
+    auto m = Matrix!(2, 2)([
+        [1, 2],
+        [3, 4]
+    ]);
+    m[0, 0] = 3.0f;
+    m[0, 1] = 4.0f;
+    m[1, 0] = 5.0f;
+    m[1, 1] = 6.0f;
+
+    assert(m[0, 0].isClose(3));
+    assert(m[0, 1].isClose(4));
+    assert(m[1, 0].isClose(5));
+    assert(m[1, 1].isClose(6));
 }
 
