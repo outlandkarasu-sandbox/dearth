93c4c0cfa8e97a0c49f4d302330ce076ace36417
outland.karasu <outland.karasu@gmail.com>
2020-07-25 14:06:39 +0900

    issue #28 add to package.

--- source/dearth/opengl/package.d
+++ source/dearth/opengl/package.d
@@ -6,6 +6,7 @@ module dearth.opengl;
 public import dearth.opengl.exception;
 public import dearth.opengl.initialize;
 public import dearth.opengl.shader;
+public import dearth.opengl.texture;
 public import dearth.opengl.types;
 public import dearth.opengl.vao;
 
