72f8f53986e711f3141a8fc553fc9a842b85663b
outland.karasu <outland.karasu@gmail.com>
2020-09-26 17:40:00 +0900

    issue #34 move ranges to utils.

--- source/dearth/shapes/utils.d
+++ source/dearth/shapes/utils.d
@@ -37,6 +37,105 @@ private:
     size_t i_;
 }
 
+/**
+Plane indices range.
+*/
+struct PlaneIndices
+{
+@nogc nothrow pure @safe:
+
+    this(size_t splitH, size_t splitV) scope
+    in (splitH > 0)
+    in (splitV > 0)
+    {
+        this.splitH_ = splitH;
+        this.splitV_ = splitV;
+        this.current_ = PlaneTriangleIndices(0, indicesH);
+    }
+
+    size_t front() const scope
+    {
+        return current_.front;
+    }
+
+    void popFront() scope
+    {
+        current_.popFront();
+        if (!current_.empty)
+        {
+            return;
+        }
+
+        ++currentH_;
+        if (currentH_ >= splitH_ && currentV_ < splitV_)
+        {
+            currentH_ = 0;
+            ++currentV_;
+        }
+
+        current_ = PlaneTriangleIndices(offsetTop, offsetBottom);
+    }
+
+    bool empty() const scope
+    {
+        return currentV_ >= splitV_;
+    }
+
+private:
+
+    size_t indicesH() const scope
+    {
+        return splitH_ + 1;
+    }
+
+    size_t offsetTop() const scope
+    {
+        return (currentV_ * indicesH) + currentH_;
+    }
+
+    size_t offsetBottom() const scope
+    {
+        return offsetTop + indicesH;
+    }
+
+    PlaneTriangleIndices current_;
+    size_t currentH_;
+    size_t splitH_;
+    size_t currentV_;
+    size_t splitV_;
+}
+
+///
+@nogc nothrow pure @safe unittest
+{
+    import std.algorithm : equal;
+
+    auto indices11 = PlaneIndices(1, 1);
+    immutable size_t[6] expected11 = [0, 1, 3, 3, 2, 0];
+    assert(indices11.equal(expected11[]));
+
+    auto indices21 = PlaneIndices(2, 1);
+    immutable size_t[12] expected21 = [
+        0, 1, 4, 4, 3, 0,
+        1, 2, 5, 5, 4, 1];
+    assert(indices21.equal(expected21[]));
+
+    auto indices12 = PlaneIndices(1, 2);
+    immutable size_t[12] expected12 = [
+        0, 1, 3, 3, 2, 0,
+        2, 3, 5, 5, 4, 2];
+    assert(indices12.equal(expected12[]));
+
+    auto indices22 = PlaneIndices(2, 2);
+    immutable size_t[24] expected22 = [
+        0, 1, 4, 4, 3, 0,
+        1, 2, 5, 5, 4, 1,
+        3, 4, 7, 7, 6, 3,
+        4, 5, 8, 8, 7, 4,
+    ];
+    assert(indices22.equal(expected22[]));
+}
+
 ///
 @nogc nothrow pure @safe unittest
 {
