834af3bde304fe4f8fd473f3962fb262c2ab0e54
outland.karasu <outland.karasu@gmail.com>
2020-09-26 16:18:58 +0900

    issue #34 add skip range.

--- source/dearth/shapes/cube.d
+++ source/dearth/shapes/cube.d
@@ -66,8 +66,8 @@ struct PlaneVertex
 
 version(unittest)
 {
-    void assertVertex(
-        scope ref const(SinglePlaneVerticesRange) r,
+    void assertVertex(R)(
+        scope ref const(R) r,
         float x, float y, size_t h, size_t v)
         @nogc nothrow pure @safe
     {
@@ -176,6 +176,121 @@ private:
     assertVertex(range, 0.5f, 1.0f, 1, 4);
     range.popFront();
     assertVertex(range, 1.0f, 1.0f, 2, 4);
+
+    range.popFront();
+    assert(range.empty);
+}
+
+struct SkipHRange(R)
+{
+    @disable this();
+
+    this()(return auto scope ref R range, size_t skipH) @nogc nothrow pure @safe scope
+    {
+        this.range_ = range;
+        this.skipH_ = skipH;
+        skip();
+    }
+
+    @property const @nogc nothrow pure @safe scope
+    {
+        PlaneVertex front()
+        in (!empty)
+        {
+            return range_.front;
+        }
+
+        bool empty()
+        {
+            return range_.empty;
+        }
+    }
+
+    void popFront() @nogc nothrow pure @safe scope
+    in (!empty)
+    {
+        range_.popFront();
+        skip();
+    }
+
+private:
+    R range_;
+    size_t skipH_;
+
+    void skip() @nogc nothrow pure @safe scope
+    {
+        while (!range_.empty && range_.front.h == skipH_)
+        {
+            range_.popFront();
+        }
+    }
+}
+
+auto skipH(R)(return auto scope ref R r, size_t skipH)
+{
+    return SkipHRange!R(r, skipH);
+}
+
+///
+@safe unittest
+{
+    auto range = SinglePlaneVerticesRange(1, 1).skipH(0);
+    assertVertex(range, 1.0f, 0.0f, 1, 0);
+    range.popFront();
+    assertVertex(range, 1.0f, 1.0f, 1, 1);
+    range.popFront();
+    assert(range.empty);
+}
+
+///
+@safe unittest
+{
+    auto range = SinglePlaneVerticesRange(2, 4).skipH(2);
+    assertVertex(range, 0.0f, 0.0f, 0, 0);
+    range.popFront();
+    assertVertex(range, 0.5f, 0.0f, 1, 0);
+
+    range.popFront();
+    assertVertex(range, 0.0f, 0.25f, 0, 1);
+    range.popFront();
+    assertVertex(range, 0.5f, 0.25f, 1, 1);
+
+    range.popFront();
+    assertVertex(range, 0.0f, 0.5f, 0, 2);
+    range.popFront();
+    assertVertex(range, 0.5f, 0.5f, 1, 2);
+
+    range.popFront();
+    assertVertex(range, 0.0f, 0.75f, 0, 3);
+    range.popFront();
+    assertVertex(range, 0.5f, 0.75f, 1, 3);
+
+    range.popFront();
+    assertVertex(range, 0.0f, 1.0f, 0, 4);
+    range.popFront();
+    assertVertex(range, 0.5f, 1.0f, 1, 4);
+
+    range.popFront();
+    assert(range.empty);
+}
+
+///
+@safe unittest
+{
+    auto range = SinglePlaneVerticesRange(2, 4).skipH(0).skipH(2);
+    assertVertex(range, 0.5f, 0.0f, 1, 0);
+
+    range.popFront();
+    assertVertex(range, 0.5f, 0.25f, 1, 1);
+
+    range.popFront();
+    assertVertex(range, 0.5f, 0.5f, 1, 2);
+
+    range.popFront();
+    assertVertex(range, 0.5f, 0.75f, 1, 3);
+
+    range.popFront();
+    assertVertex(range, 0.5f, 1.0f, 1, 4);
 
     range.popFront();
     assert(range.empty);
