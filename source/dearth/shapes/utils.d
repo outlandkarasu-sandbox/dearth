df6afc68667ead3da068ea7b71e69e871bd647b6
outland.karasu <outland.karasu@gmail.com>
2020-12-19 14:14:51 +0900

    issue #34 add plane points.

--- source/dearth/shapes/utils.d
+++ source/dearth/shapes/utils.d
@@ -14,17 +14,18 @@ struct PlaneTrianglePoints
 {
 @nogc nothrow pure @safe:
 
-    this(size_t offsetX, size_t offsetY) scope
-    {
-        this.offsetX_ = offsetX;
-        this.offsetY_ = offsetY;
-    }
-
     Point front() const scope
     in (!empty)
     {
-        immutable p = frontWithoutOffset;
-        return Point(p.x + offsetX_, p.y + offsetY_);
+        switch (current_)
+        {
+            default: case 0: return Point(0, 0);
+            case 1: return Point(1, 0);
+            case 2: return Point(0, 1);
+            case 3: return Point(1, 0);
+            case 4: return Point(1, 1);
+            case 5: return Point(0, 1);
+        }
     }
 
     void popFront() scope
@@ -40,22 +41,7 @@ struct PlaneTrianglePoints
 
 private:
     enum LENGTH = 6;
-    size_t offsetX_;
-    size_t offsetY_;
     uint current_;
-
-    Point frontWithoutOffset() const scope
-    {
-        switch (current_)
-        {
-            default: case 0: return Point(0, 0);
-            case 1: return Point(1, 0);
-            case 2: return Point(0, 1);
-            case 3: return Point(1, 0);
-            case 4: return Point(1, 1);
-            case 5: return Point(0, 1);
-        }
-    }
 }
 
 ///
@@ -71,32 +57,62 @@ private:
         Point(1, 1),
         Point(0, 1),
     ]));
+}
 
-    assert(PlaneTrianglePoints(3, 0).equal([
-        Point(3, 0),
-        Point(4, 0),
-        Point(3, 1),
-        Point(4, 0),
-        Point(4, 1),
-        Point(3, 1),
-    ]));
+struct PlanePoints
+{
+@nogc nothrow pure @safe:
 
-    assert(PlaneTrianglePoints(0, 3).equal([
-        Point(0, 3),
-        Point(1, 3),
-        Point(0, 4),
-        Point(1, 3),
-        Point(1, 4),
-        Point(0, 4),
-    ]));
+    this(size_t width, size_t height) scope
+    {
+        this.width_ = width;
+        this.height_ = height;
+    }
 
-    assert(PlaneTrianglePoints(3, 3).equal([
-        Point(3, 3),
-        Point(4, 3),
-        Point(3, 4),
-        Point(4, 3),
-        Point(4, 4),
-        Point(3, 4),
+    Point front() const scope
+    in (!empty)
+    {
+        return Point(x_, y_);
+    }
+
+    void popFront() scope
+    in (!empty)
+    {
+        ++x_;
+
+        if (x_ >= width_)
+        {
+            x_ = 0;
+            ++y_;
+        }
+    }
+
+    bool empty() const scope
+    {
+        return y_ == height_;
+    }
+
+private:
+    size_t width_;
+    size_t height_;
+    size_t x_;
+    size_t y_;
+}
+
+///
+@nogc nothrow pure @safe unittest
+{
+    import std.algorithm : equal;
+
+    assert(PlanePoints().empty);
+    assert(PlanePoints(1, 1).equal([Point(0, 0)]));
+    assert(PlanePoints(2, 1).equal([Point(0, 0), Point(1, 0)]));
+    assert(PlanePoints(1, 2).equal([Point(0, 0), Point(0, 1)]));
+    assert(PlanePoints(2, 2).equal([
+        Point(0, 0),
+        Point(1, 0),
+        Point(0, 1),
+        Point(1, 1),
     ]));
 }
 
